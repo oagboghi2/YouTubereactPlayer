7980525e378b0b8e7ff3f04f241362b22d6ad178
Obo Agboghidi <oagboghi2@gmail.com>
2018-10-25 23:14:46 -0700

    resetted app project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//Create a new component. This component should produce some HTML
+
+// Take this component generated HTML and put it on the page( in the DOM)
+
+class App extends Component {
+  render() {
+    return <div>Hi!</div>
+  }
+}
+
+export default App;
